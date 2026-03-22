4a37badc321209726887b8865a10fea7148667b2
brahiam <lbrahianvr123@gmail.com>
2026-03-22 00:33:52 -0400

    fix

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-echo "Waiting for PostgreSQL..."
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+echo "Waiting for PostgreSQL at ${POSTGRES_HOST}:${POSTGRES_PORT}..."
+while ! nc -z "${POSTGRES_HOST}" "${POSTGRES_PORT}"; do
   sleep 1
 done
 echo "PostgreSQL is ready!"
